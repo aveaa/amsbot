82bcabebbec165bfd23bb4eff88ab09b0373e31c
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-09-09 10:19:30 +0300

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,5 +1,10 @@
 exports.info = {description: 'Показать пинг бота', required: 'SEND_MESSAGES'}
 exports.run = (client, msg, args) => {
+  var emojis = {
+  zapret: '488237552545955840',
+  razreshenie: '488238174401986560'
+}
+  
   var razreshenie = client.emojis.get(emojis.razreshenie);
   var embed = new client.RichEmbed()
     .setTitle('Пинг')
