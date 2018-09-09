517f476619b24d65d08c7467e4bd0b0ff2fa243d
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-09-09 09:58:39 +0300

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,8 +1,9 @@
 exports.info = {description: 'Показать пинг бота', required: 'SEND_MESSAGES'}
 exports.run = (client, msg, args) => {
+  var razreshenie = client.emojis.get(emojis.razreshenie);
   var embed = new client.RichEmbed()
     .setTitle('Пинг')
-    .setDescription('${client.emojis.get(emojis.razreshenie)} Пинг: ${Math.round(client.ping)}ms`)    
+    .setDescription('${razreshenie} Пинг: ${Math.round(client.ping)}ms`)    
     .setColor('FFFFFF')
   msg.channel.send({embed})
 }
