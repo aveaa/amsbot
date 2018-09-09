0955c822f763c7e697a13e60558d9b9646a02c93
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-09-09 10:15:49 +0300

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -3,7 +3,7 @@ exports.run = (client, msg, args) => {
   var razreshenie = client.emojis.get(emojis.razreshenie);
   var embed = new client.RichEmbed()
     .setTitle('Пинг')
-    .setDescription('${razreshenie} Пинг: ${Math.round(client.ping)}ms`)    
+    .setDescription('${razreshenie} Пинг: ${Math.round(client.ping)}ms')    
     .setColor('FFFFFF')
   msg.channel.send({embed})
 }
