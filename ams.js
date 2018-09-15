26ab5ccc1427110b01e3ded3393acd2300df0875
megafreezer <36858947+megafreezer@users.noreply.github.com>
2018-09-15 17:29:49 +0300

    Update ams.js

--- ams.js
+++ ams.js
@@ -1,15 +1,10 @@
 var Discord = require('discord.js')
 var client = new Discord.Client()
 
-var emojis = {
-  zapret: '488237552545955840',
-  razreshenie: '488238174401986560'
-}
-
 require('events').EventEmitter.prototype._maxListeners = 100
 client.p = '#'
 client.sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-client.owners = ['341988428457705482', '406343162651738112']
+client.owners = ['341988428457705482']
 client.loggers = {
   'iplogger.com': ['iplogger.com', 'iplogger.org', 'iplogger.ru', '2no.co', 'yip.su'],
   'grabify.link': ['grabify.link', 'starbucksisbadforyou.com', 'bmwforum.co', 'leancoding.co', 'quickmessage.io', 'spottyfly.com', 'spötify.com', 'stopify.co', 'yoütu.be', 'yoütübe.co', 'yoütübe.com', 'xda-developers.io', 'starbucksiswrong.com', 'starbucksisbadforyou.com', 'bucks.as', 'discörd.com', 'minecräft.com', 'cyberh1.xyz', 'discördapp.com', 'freegiftcards.co', 'disçordapp.com', 'iany.pl', 'my-alts.eu', 'l-imgur.pl', 'exec-true.eu', 'tigercore.eu']
@@ -18,16 +13,6 @@ client.RichEmbed = Discord.RichEmbed
 
 client.on('ready', () => {
   console.log(client.user.tag);
-})
-
-client.on('guildMemberAdd', member => {
-  member.addRole('487705411172958218')
-  var embed = new Discord.RichEmbed()
-    .setTitle('Добро пожаловать на сервер Sahara')
-    .setDescription('Для дальнейшего общения просим прочитать <#486957746957189131>. Там находится вся необходимая информация.')
-    .setFooter('Спасибо, что присоединились именно к нам')
-    .setThumbnail('https://cdn.discordapp.com/attachments/486958324898594836/488046884791910413/8acbf6ccf537452991522d510e8a0bdc_2181.png')
-  member.send({embed})
 })
 
 client.on('message', msg => {
